1fe39729dcfe7cab4acb9dce1980c1a16ca351f6
andrex3y <regmen68@gmail.com>
2022-05-01 14:11:42 +0300

    updatepin

---
+++ Settings.py
@@ -0,0 +1,14 @@
+from pygame import *
+
+# игровая сцена:
+back = (200, 255, 255)  # цвет фона (background)
+win_width = 600
+win_height = 500
+window = display.set_mode((win_width, win_height))
+window.fill(back)
+
+# флаги, отвечающие за состояние игры
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
